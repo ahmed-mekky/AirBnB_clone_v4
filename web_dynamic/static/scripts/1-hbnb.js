c889dd11c29a7454e53e2f832709c424ff4be974
AhmedEhab2022 <am8122004@gmail.com>
2024-02-10 05:05:02 -0800

    1-hbnb.js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  let amenitiesChecked = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      amenitiesChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesChecked[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
+  });
+});
