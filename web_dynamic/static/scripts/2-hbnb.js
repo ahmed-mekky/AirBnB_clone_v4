b61d82b744ef96ba9b83918c187e6419f8f23717
AhmedEhab2022 <am8122004@gmail.com>
2024-02-10 07:53:09 -0800

    3. API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  let amenitiesChecked = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      amenitiesChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesChecked[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
